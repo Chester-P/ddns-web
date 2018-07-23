63a9c6a266b90fd05047403ae00e84168937814e
Chester Pang <i@bopa.ng>
2018-07-23 16:35:37 +1000

    introduce sqlalchemy

--- ddns.py
+++ ddns.py
@@ -2,31 +2,61 @@ import subprocess
 import re
 import datetime
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker
 
 NS_QUERY_CMD = \
     'dig +nostats +nocomments +nocmd -b 127.0.0.1 @localhost {} axfr'
 
-
-class Record:
-    def __init__(self, _id, SOA_serial, FQDN, TTL, _type, value,
-                 created_by, zone, create_time=False):
-        self._id = _id
-        self.SOA_serial = SOA_serial
-        self.FQDN = FQDN
-        self.TTL = TTL
-        self._type = _type
-        self.value = value
-        self.created_by = created_by
-        self.zone = zone
-        self.create_time = create_time
+engine = create_engine('sqlite:///ddns-test.db', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-class Zone:
-    def __init__(self, _id, name, domain, created_by):
-        self.records = []
-        self.domain = domain
-        self.name = name
-        self.created_by = created_by
+class Record(Base):
+    __tablename__ = 'record'
+    record_id = Column(Integer, primary_key=True)
+    SOA_serial = Column(Integer)
+    FQDN = Column(String)
+    TTL = Column(Integer)
+    type = Column(String)
+    value = Column(String)
+    create_time = Column(DateTime)
+    created_by = Column(Integer, ForeignKey('user.uid'))
+    zone = Column(Integer, ForeignKey('zone.zone_id'))
+
+    # def __init__(self, _id, SOA_serial, FQDN, TTL, _type, value,
+    #              created_by, zone, create_time=False):
+    #     self._id = _id
+    #     self.SOA_serial = SOA_serial
+    #     self.FQDN = FQDN
+    #     self.TTL = TTL
+    #     self._type = _type
+    #     self.value = value
+    #     self.created_by = created_by
+    #     self.zone = zone
+    #     self.create_time = create_time
+
+    def __repr__(self):
+        return '<Record(id={}, SOA_serial={}, FQDN={}, type={}, value={})>' \
+               .format(self.record_id, self.SOA_serial,
+                       self.FQDN, self.type, self.value)
+
+
+class Zone(Base):
+    __tablename__ = 'zone'
+    zone_id = Column(Integer, primary_key=True)
+    created_by = Column(Integer, ForeignKey('user.uid'))
+    domain = Column(String)
+    name = Column(String)
+    # def __init__(self, _id, name, domain, created_by):
+    #     self.records = []
+    #     self.domain = domain
+    #     self.name = name
+    #     self.created_by = created_by
 
     def addRecord(self, Record):
         pass
@@ -46,15 +76,33 @@ class Zone:
         records = [re.split('\t+', line)
                    for line in output.decode().split('\n') if line]
         assert(records)
-        self.records.clear()
         SOA_serial = records[0][4].split()[0]
         i = 0
         for record in records:
-            obj = Record(i, SOA_serial, record[0], record[1],
-                         record[3], record[4], -1, self,
-                         datetime.datetime.now)
-            self.records.append(obj)
+            obj = Record(
+                    record_id=i,
+                    SOA_serial=SOA_serial,
+                    FQDN=record[0],
+                    TTL=record[1],
+                    type=record[3],
+                    value=record[4],
+                    create_time=datetime.datetime.now(),
+                    created_by=-1,
+                    zone=1
+                    )
+            session.add(obj)
             i += 1
+        session.commit()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    uid = Column(Integer, primary_key=True)
+    username = Column(String)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
 
 class NsupdateTransaction:
     def __init__(self):
@@ -64,3 +112,17 @@ class NsupdateTransaction:
     # execute nsupdate
     def execute(self):
         pass
+
+
+if __name__ == '__main__':
+    # create schema
+    Base.metadata.create_all(engine)
+
+    anonymousUser = User(uid=-1, username='unknown',
+                         name='unknown', email='unknown')
+    test_zone = Zone(zone_id=1, created_by=-1,
+                     domain='bopa.ng', name='test_zone')
+    session.add(anonymousUser)
+    session.add(test_zone)
+    session.commit()
+    test_zone.refresh()
